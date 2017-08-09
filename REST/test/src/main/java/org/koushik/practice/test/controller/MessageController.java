6c70f4676801c31eefa7eaab53cfd2e55936e78b
koushiksudheendra <koushik.ckm@gmail.com>
2017-08-09 09:09:45 +0530

    commit

--- REST/test/src/main/java/org/koushik/practice/test/controller/MessageController.java
+++ REST/test/src/main/java/org/koushik/practice/test/controller/MessageController.java
@@ -2,6 +2,7 @@ package org.koushik.practice.test.controller;
 
 import java.util.List;
 
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,10 +11,21 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.koushik.practice.test.model.Message;
+import org.koushik.practice.test.model.MessageFilterBean;
 import org.koushik.practice.test.service.MessageService;
+
+
+/*
+ * 
+ * @QueryParam
+ * @BeanParam
+ * 
+ */
+
 
 @Path("messages")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,8 +34,29 @@ public class MessageController {
 
 	MessageService messageService = new MessageService();
 	
-	@GET
-	public List<Message> getMessages(){
+	@GET 
+	public List<Message> getMessages(@QueryParam("year") int year,
+			@QueryParam("start") int start,
+			@QueryParam("size") int size){
+	/*
+	 * Instead of multiple query params, we can also use BeanParam to access all query params
+	 */
+	//public List<Message> getMessages(@BeanParam MessageFilterBean filterBean){
+
+		if (year>0){
+			return messageService.getAllMessagesForYear(year);
+		}
+		if(start>=0 && size>=1){
+			return messageService.getAllMessagesPaginated(start, size);
+		}
+		
+		/*if (filterBean.getYear()>0){
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		}
+		if(filterBean.getStart()>=0 && filterBean.getSize()>=1){
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		}*/
+		
 		return messageService.getAllMessages();
 	}
 	
@@ -49,5 +82,20 @@ public class MessageController {
 	@Path("/{messageId}")
 	public Message removeMessage(@PathParam("messageId") long messageId){
 		return messageService.removeMessage(messageId);
+	}
+	
+	
+	/*
+	 * 
+	 * 
+	 * Implementing sub resources
+	 * The below method will be mapped to the api in CommentsController
+	 * the call to below path will call the api in CommentsController
+	 * 
+	 */
+	
+	@Path("/{messageId}/comments")
+	public CommentsController getCommentsResource(){
+		return new CommentsController();
 	}
 }
