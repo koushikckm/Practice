a213774e6fa44f5156a3a0b2ed3e3ecce29ecaf4
koushiksudheendra <koushik.ckm@gmail.com>
2017-08-16 14:45:32 +0530

    commit

---
+++ Struts2/Struts2Starter/src/org/koushik/struts2/action/LoginAction.java
@@ -0,0 +1,36 @@
+package org.koushik.struts2.action;
+
+public class LoginAction {
+
+	private String userId;
+	private String password;
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String execute(){
+		
+		if(getUserId().equals("userId") && getPassword().equals("password")){
+			return "success";
+		}
+		return "failure";
+	}
+}
---
+++ Struts2/Struts2Starter/src/org/koushik/struts2/service/TutorialFinderService.java
@@ -0,0 +1,13 @@
+package org.koushik.struts2.service;
+
+public class TutorialFinderService {
+
+	public String getBestTutorialService(String language){
+		if(language.equals("java")){
+			return "Java Brains!!";
+		}
+		else{
+			return "Language not supported";
+		}
+	}
+}
